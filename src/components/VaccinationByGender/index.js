870c3c17ebc18e02a0bba12376b3bec0518a0778
DaveedGangi <daveedgangi@gmail.com>
2023-12-08 13:19:40 +0000

    Tests

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,6 +1,6 @@
 // Write your code here
 
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 const GenderBy = props => {
   const {GenderData} = props
@@ -8,25 +8,23 @@ const GenderBy = props => {
     <div>
       <h1>Vaccination by gender</h1>
 
-      <ResponsiveContainer height={600} width="100%">
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="30%"
-            data={GenderData}
-            startAngle={0}
-            endAngle={180}
-            innerRadius="40%"
-            outerRadius="60%"
-            dataKey="count"
-          >
-            <Cell name="male" fill=" #94a3b8" />
-            <Cell name="female" fill=" #5a8dee" />
-            <Cell name="others" fill="#cbd5e1" />
-          </Pie>
-          <Legend iconType="circle" verticalAlign="middle" align="center" />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart height={600} width="100%">
+        <Pie
+          cx="70%"
+          cy="30%"
+          data={GenderData}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="male" fill=" #94a3b8" />
+          <Cell name="female" fill=" #5a8dee" />
+          <Cell name="others" fill="#cbd5e1" />
+        </Pie>
+        <Legend iconType="circle" verticalAlign="middle" align="center" />
+      </PieChart>
     </div>
   )
 }
--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,51 +1,34 @@
 // Write your code here
 
-import {
-  Bar,
-  BarChart,
-  XAxis,
-  YAxis,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts'
+import {Bar, BarChart, XAxis, YAxis, Legend} from 'recharts'
 
 const CoverageBy = props => {
   const {CoverSending} = props
 
-  const DataFormatter = number => {
-    if (number > 1000) {
-      return `${(number / 1000).toString()}k`
-    }
-    return number.toString()
-  }
   return (
     <div>
       <div>
         <h1>Vaccination Coverage</h1>
       </div>
-      <ResponsiveContainer height={440} width="100%">
-        <BarChart data={CoverSending} margin={{top: 5}}>
-          <XAxis
-            dataKey="name"
-            tick={{
-              stroke: 'gray',
-              strokeWidth: 1,
-            }}
-          />
-          <YAxis
-            tick={{stroke: 'gray', strokeWidth: 0}}
-            tickFormatter={DataFormatter}
-          />
-          <Legend
-            wrapperStyle={{
-              padding: 30,
-            }}
-          />
 
-          <Bar dataKey="doseOne" name="Dose 1" fill="#a3df9f" barSize="20%" />
-          <Bar dataKey="doseTwo" name="Dose 2" fill="#2cc6c6" barSize="20%" />
-        </BarChart>
-      </ResponsiveContainer>
+      <BarChart data={CoverSending} height={500} width="100%" margin={{top: 5}}>
+        <XAxis
+          dataKey="name"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis tick={{stroke: 'gray', strokeWidth: 0}} />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+
+        <Bar dataKey="doseOne" name="Dose 1" fill="#a3df9f" barSize="20%" />
+        <Bar dataKey="doseTwo" name="Dose 2" fill="#2cc6c6" barSize="20%" />
+      </BarChart>
     </div>
   )
 }
